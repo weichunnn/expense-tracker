d86afcdb8ea14f7f20555c1888b1648db6721e30
WeiChunnn <tanweichun@outlook.com>
2020-07-24 13:06:22 +0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const colors = require('colors');
+const morgan = require('morgan');
+const connectDB = require('./config/db');
+require('dotenv').config({ path:'./config/config.env' })
+
+connectDB();
+
+const transactions = require('./routes/transactions');
+
+const app = express();
+
+app.use('/api/v1/transactions', transactions);
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, console.log(`Server running smoothly in ${process.env.NODE_ENV} mode on port: ${PORT}`.yellow.bold));
