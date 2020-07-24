fef8bfaa6bf8e87608f4beb812055b43d492e55b
WeiChunnn <tanweichun@outlook.com>
2020-07-24 13:52:59 +0800

    Added Transaction Model

---
+++ models/Transaction.model.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const TransactionSchema = new Schema ({
+    text: {
+        type: String,
+        trim: true,
+        required: [true, 'Please add some text']
+    },
+    amount: {
+        type: Number,
+        required: [true, 'Please add a positive or negative integer']
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+module.exports = mongoose.model('Transaction', TransactionSchema);
