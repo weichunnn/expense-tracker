267821c6273f8d1ef5ce2c1cfe2536b46c6aede7
WeiChunnn <tanweichun@outlook.com>
2020-07-24 18:02:52 +0800

    Updated responses of controllers

--- controllers/transactions.controller.js
+++ controllers/transactions.controller.js
@@ -1,20 +1,72 @@
+const Transaction = require('../models/Transaction.model');
+
 // @ desc Get all transactions
 // @route GET /api/v1/transactions
 // @access Public
-exports.getTransactions = (req, res,next) => {
-    res.send('GET transactions');
+exports.getTransactions = async (req, res,next) => {
+    try {
+        const transactions = await Transaction.find();
+
+        return res.status(200).json({
+            success: true,
+            count: transactions.length,
+            data: transactions
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            error: 'Server Error'
+        });
+    } 
 }
 
 // @ desc Add all transactions
 // @route POST /api/v1/transactions
 // @access Public
-exports.addTransactions = (req, res,next) => {
-    res.send('POST transactions');
-}
+exports.addTransactions = async (req, res,next) => {
+    try {
+        const {text, amount} = req.body;
+
+        const transaction = await Transaction.create(req.body);
+
+        return res.status(201).json({
+        success: true,
+        data: transaction
+        })
+    } catch (error) {
+        return res.status(400).json({
+            success: false,
+            error: 'Client Error'
+        });       
+     }
+        
+    }
 
 // @ desc Delete all transactions
 // @route DELETE /api/v1/transactions/:id
 // @access Public
-exports.deleteTransactions = (req, res,next) => {
-    res.send('DELETE transactions');
+exports.deleteTransactions = async (req, res,next) => {
+     try {
+         const transaction = await Transaction.findById(req.params.id);
+
+         if (!transaction) {
+             return res.status(404).json({
+                 success: false,
+                 error: 'No transaction found'
+             });
+         }
+
+         await transaction.remove();
+
+         return res.status(200).json({
+            success: false,
+            data: {}
+         })
+
+     } catch (error) {
+        return res.status(500).json({
+            success: false,
+            error: 'Server Error'
+        });
+    } 
 }
