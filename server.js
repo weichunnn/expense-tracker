51598b92f5962afd58a3b6e2529001433656d8ef
WeiChunnn <tanweichun@outlook.com>
2020-07-25 00:30:08 +0800

    Added comments

--- server.js
+++ server.js
@@ -20,9 +20,14 @@ if (process.env.NODE_ENV === 'development') {
 
 app.use('/api/v1/transactions', transactions);
 
+// Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
+    //Set static folder
     app.use(express.static('client/build'));
-    app.get('*', (req, res) => {res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))})
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
 }
 
 const PORT = process.env.PORT || 5000
